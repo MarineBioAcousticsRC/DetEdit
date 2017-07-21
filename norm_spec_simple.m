3906899fa28c9d6d1c685e6fa6b961e00da68f60
asolsonaberga <asolsonaberga@ucsd.edu>
2017-07-21 16:35:33 -0700

    Fix last bug in spec norm

--- norm_spec_simple.m
+++ norm_spec_simple.m
@@ -6,5 +6,5 @@ mxSPEC = max(SPEC(:,1:flowt),[],2);
 SPEC = SPEC ./ repmat(mxSPEC,1,size(spectraSet,2));  % make high freq part = 1
 
 meanSPEC = 20*log10(nanmean(10.^SPEC./20,1));
-meanMin = meanSPEC-min(meanSPEC(1:flowt));
-meanSPEC = meanMin./max(meanMin(1:flowt));
+meanMin = meanSPEC-min(meanSPEC);
+meanSPEC = meanMin./max(meanMin);

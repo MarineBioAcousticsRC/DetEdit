598f0818109761be72f33b41fb4d9a755db8679a
asolsonaberga <asolsonaberga@ucsd.edu>
2017-08-15 11:29:40 -0700

    Ignore changes in detEdit settings

--- detEdit_settings.m
+++ detEdit_settings.m
@@ -18,7 +18,7 @@ sp = 'Pm'; % species code (can be: 'Ko' or 'k' (kogia);
 % 'De' (Dolphin), 'Po' (porpoise), 'MFA', 'whs' (whistles), 'Dl' (beluga)
 c4fd = 100; % Interval to check for false detections
 sdir = 'E:\TPWS'; %Directory with TPWS files
-tfName = 'C:\MATLAB\transfer_functions'; % Directory ...
+tfName = 'C:\Users\Alba\Documents\MATLAB\transfer_functions'; % Directory ...
 % with .tf files (directory containing folders with different series ...
 % (e.g. 300_series,400_series)
 
@@ -70,7 +70,7 @@ maxDetLoad = 4e5; % [] - read all or 4e5 - the number of detections above
 % timeseries into memory this is for large files (e.g. dolphin click detections)
 % if maxDetLoad exist, plotaxes can be defined to keep the format of plot
 % 51 and 53
-% plotaxes.minRMS = 60;
-% plotaxes.maxRMS = 130;
-% plotaxes.minPP = 120;
-% plotaxes.maxPP = 180;
+plotaxes.minRMS = 60;
+plotaxes.maxRMS = 130;
+plotaxes.minPP = 120;
+plotaxes.maxPP = 180;

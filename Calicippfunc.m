2a9acea7bc181c66805ac00462b8d5459ea5907e
Alba Solsona <alba.solb@gmail.com>
2018-02-01 14:38:58 -0800

    histogram bars adjusted to work with  threh in settings

--- Calicippfunc.m
+++ Calicippfunc.m
@@ -53,9 +53,12 @@ mepp = median(MPP);
 
 % Plot histogram
 h23 = figure(23);
-nbinsdb = (p.dbRange(1):p.dbRange(2));
-[y,centers] = hist(MPP,nbinsdb);
-bar(centers,y)
+% nbinsdb = (p.dbRange(1):p.dbRange(2));
+% [y,centers] = hist(MPP,nbinsdb);
+% bar(centers,y)
+center = p.threshRL:1:170;
+[nhist] = histc(MPP,center);
+bar(center, nhist, 'barwidth', 1, 'basevalue', 1)
 title(sprintf('N=%d',length(MPP)))
 xlabel('Peak-Peak Amplitude (dB)')
 

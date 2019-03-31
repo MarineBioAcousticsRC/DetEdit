93d18d8ecbfe5249f6f0c7bb828d47ec96578469
Alba Solsona <alba.solb@gmail.com>
2019-03-31 16:35:33 -0700

    wiki update and settings file to myDataSettings

--- Settings/myDataSettings.m
+++ Settings/myDataSettings.m
@@ -1,65 +1,74 @@
-% detEdit_settings
-% inputs can be defined here instead of through gui windows
+% yourDataSettings
 
-% You can make different versions of this, with different names
+% Example script to define directories and parameter preference for the
+% interface. You can make different versions of this, with different names
 % for different species or sites.
-%
-% eg. detEdit_settings_kogia.m
-% 
-% Then change line 22 of detEdit to match the settings file you want to
-% use.
 
-filePrefix = 'GOM_MC_11_disk12'; % File name to match. 
-% File prefix should include deployment, site, (disk is optional). 
-% Example: 
-% File name 'GofMX_DT01_disk01-08_TPWS2.mat' 
-%                    -> filePrefix = 'GofMX_DT01_disk01-08'
-itnum = '2'; % iteration
-srate = 200; % sample rate
-sp = 'Pm'; % species code (can be: 'Ko' or 'k' (kogia);
-% 'Zc' or 'z' (Cuvier's),'Me' or 'm' (Gervais'), 'Md' (Blainville's), BWG,...
-% 'De' (Dolphin), 'Po' (porpoise), 'MFA', 'whs' (whistles), 'Dl' (beluga)
-sdir = 'H:\newTPWS'; %Directory with TPWS files
-tfName = ''; %'E:\TF_files'; % Directory ...
-% with .tf files (directory containing folders with different series ...
-% (e.g. 300_series,400_series)
+% Define input/output locations.REQUIRED
+filePrefix = 'Filename'; % TPWS file name to match. 
+% Optional, replace file prefix to a more generic name to specify settings 
+% for mkLTSAsessions or modDet, it will run in multiple files.
+% Example: GofMX_DT03 (will run modDet to all files matching the generic name) 
+iterationNum = '1'; % iteration number
+sampleRate = 200; % replace with your sample rate
+sp = ''; % species code 
+% Example:  '' (Unknown), 'De' (Dolphin), 'Pm' (sperm whale)
+% (See comments in initSpParams.m for more species codes)
+tpwsDir = 'E:\MyTPWSfolder'; % identify folder containing TPWS files
+
+% Specific input/output locations (comment them if not in use)
+% tfName = 'E:\MyTFfolder'; % identify folder containing transfer function 
+% files (.tf). Required if spectra has not been calculated peak to received levels 
+
+ltsaDir = 'E:\MyLTSAfolder'; % identify folder containing ltsa files (.ltsa)
+% REQUIRED to run mkLTSAsessions.m
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Settings preferences to override defaults parameters for the interface
-% Uncomment these in as needed to override detEdit defaults defined at
-% initDefaultParams
+%%% Setting preferences to override defaults parameters for the interface
+% (Uncomment these in as needed to override detEdit defaults defined at
+% initDefaultParams.m and initSpParams.m)
 
 %% Bout preferences
-% paramsUser.threshRL = 130; % minimum RL threshold in dB peak-to-peak
+% paramsUser.threshRL = 0; % minimum RL threshold in dB peak-to-peak
 % paramsUser.tfSelect = 0; % freq used for transfer function, leave at 0 if no adjustment
 % paramsUser.minBout = 0;% minimum bout duration in seconds
-% paramsUser.p.gth = .5;    % gap time in hrs between sessions
+% paramsUser.gth = .5;    % gap time in hrs between sessions
 % paramsUser.binDur = 5; % bin duration in minutes
 % paramsUser.dfManual = []; % LTSA step size in 10 [Hz] bins
 % paramsUser.specploton = 1; %(1 = yes | 0 = no ) spectra parameters plot
-% paramsUser.p.minNdet = 1; % minimum detections per session.
+% paramsUser.minNdet = 1; % minimum detections per session.
 % paramsUser.maxDetLoad = 4e5; % ([] - all) max detections to read from disk ( recommended for large files)
-% paramsUser.c4fd = 1000; % Detections step size to estimate false detection rate
+% paramsUser.c4fd = 1; % Detections step size to estimate false detection rate
 
 %% Panel LTSA and time series
 % paramsUser.rlLow = 110; % PP plot window low limit
 % paramsUser.rlHi = 170; % PP plot window high limit
 % paramsUser.ltsaContrast = 250; % ltsa contrast
 % paramsUser.ltsaBright = 100; % ltsa brightness
-% paramsUser.ltsaLims = [0,srate/2]; % max and min of LTSA plot
+% paramsUser.ltsaLims = [0,sampleRate/2]; % max and min of LTSA plot
 % paramsUser.ltsaMax = 6; % ltsa maximum duration per session
 % paramsUser.dtHi = .5; % max yaxis value for ICI display in sec
 % paramsUser.minDur = []; % minimum window duration (if specified in minutes)
 
 %% Panel Frequency spectra
 % paramsUser.fLow = 0; % Minimum frequency of interest
-% paramsUser.fHi = srate/2; % Maximum frequency of interest
+% paramsUser.fHi = sampleRate/2; % Maximum frequency of interest
 
 %% Panel RL rms vs. RL pp | Peak freq.
-% paramsUser.slope = 0.7; % slope for shifting data vertically
+% paramsUser.slope = 1; % slope for shifting data vertically
 % paramsUser.threshRMS = 0; % default for < command, RMS threshold cutoff
 % paramsUser.threshPP = 0; % default for : command, PP threshold cutoff
-% paramsUser.threshHiFreq = 40; % default for ^ command, high freq cutoff for clicks
+% paramsUser.threshHiFreq = 0; % default for ^ command, high freq cutoff for clicks
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Setting preferences to override modDet.m defaults
+% (Uncomment these in as needed to override detEdit defaults defined at
+% initDefaultParams.m)
+
+% paramsUser.excludeID = 0; % yes - 1 | no - 0. Exclude ID times from MTT files 
+% paramsUser.calcParams = 0;% yes - 1 | no - 0. Calculate Parameters peak-to-peak, 
+% inter-detection-interval and peak frequency
+
+%% ID labels legend
+% Assign an ID to colors
+paramsUser.mySpID = {'Sperm Whale','Dolphin','Beaked Whale', 'Echosounder', 'Ship'};

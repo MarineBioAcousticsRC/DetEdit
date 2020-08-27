7baf565bd0e0eb96dce5d965ee9e0da66a00460c
Rebecca Cohen <30301737+rcohen85@users.noreply.github.com>
2020-08-27 14:29:18 -0700

    Update dolphinSettings.m

--- Settings/dolphinSettings.m
+++ Settings/dolphinSettings.m
@@ -5,7 +5,7 @@
 % for different species or sites.
 
 % Define input/output locations.REQUIRED
-filePrefix = 'NFC_A_03_disk01a'; % TPWS file name to match. 
+filePrefix = 'WAT_BC_01_disk01a'; % TPWS file name to match. 
 % Optional, replace file prefix to a more generic name to specify settings 
 % for mkLTSAsessions or modDet, it will run in multiple files.
 % Example: GofMX_DT03 (will run modDet to all files matching the generic name) 
@@ -14,13 +14,13 @@ sampleRate = 200; % replace with your sample rate
 sp = 'De'; % species code 
 % Example:  '' (Unknown), 'De' (Dolphin), 'Pm' (sperm whale)
 % (See comments in initSpParams.m for more species codes)
-tpwsDir = 'F:\NFC_A_03\TPWS'; % identify folder containing TPWS files
+tpwsDir = 'G:\WAT_BC_01\WAT_BC_01_TPWS'; % identify folder containing TPWS files
 
 % Specific input/output locations (comment them if not in use)
 % tfName = 'E:\MyTFfolder'; % identify folder containing transfer function 
 % files (.tf). Required if spectra has not been calculated peak to received levels 
 
-ltsaDir = 'F:\NFC_A_03\LTSAs'; % identify folder containing 
+ltsaDir = 'G:\WAT_BC_01\WAT_BC_01_LTSA'; % identify folder containing 
 % ltsa files (.ltsa). REQUIRED to run mkLTSAsessions.m
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -38,7 +38,7 @@ paramsUser.minBout = 0;% minimum bout duration in seconds
 % paramsUser.specploton = 1; %(1 = yes | 0 = no ) spectra parameters plot
 % paramsUser.minNdet = 1; % minimum detections per session.
 % paramsUser.maxDetLoad = 4e5; % ([] - all) max detections to read from disk ( recommended for large files)
-% paramsUser.c4fd = 1000; % Detections step size to estimate false detection rate
+ paramsUser.c4fd = 1000; % Min # detections to test to estimate false detection rate
 
 %% Panel LTSA and time series
 paramsUser.rlLow = 115; % PP plot window low limit
